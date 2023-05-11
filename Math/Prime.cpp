3d4c4cc79f964027808bca3001d0a9cd7bb6aaf5
Eric <101823195+erichung1113@users.noreply.github.com>
2023-05-12 02:09:58 +0800

    Update Prime.cpp

--- Math/Prime.cpp
+++ Math/Prime.cpp
@@ -1,11 +1,12 @@
-vector<bool> isPrime;
+vector<bool> isPrime(MAXN, true);
+vector<int> Prime;
 
-void Sieve(int N) {
-    isPrime.resize(N+1,true);
+void Sieve() {
     isPrime[0] = isPrime[1] = false;
-    for (int i = 2; i <= N; i++) {
+    for (int i = 2; i < MAXN; i++) {
         if (isPrime[i]) {
-            for (int j = i * i; j < N; j += i) {
+            Prime.push_back(i);
+            for (int j = i * i; j < MAXN; j += i) {
                 isPrime[j] = false;
             }
         }
